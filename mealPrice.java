9d803b74e122367946edf609099dbb9adb11775d
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-12-21 12:33:48 -0500

    pseudo code for meal price

--- mealPrice.java
+++ mealPrice.java
@@ -1,3 +1,11 @@
+// pseudo code for mealPrice.java
+
+// • Step 1 - Calculate Tip: tip = tipRate * listedMealPrice;
+// • Step 2 - Calculate Tax: tax = taxRate * listedMealPrice;
+// Step 3 - Calculate Result: result = tip + tax +
+// listedMealPrice;
+// • Step 4 - Output Result: System.out.println(result);
+
 public class Main {
 
   public static void calculateTotalMealPrice(double listedMealPrice,

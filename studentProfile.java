8e3565a6bf12dd301648b4c4da786a10ef291294
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2021-12-21 12:29:18 -0500

    studentprofile

---
+++ studentProfile.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        StudentProfile profileOne = new StudentProfile("Salman", "Sal", "Filly", 3.75, 2022);
+        StudentProfile profileTwo = new StudentProfile("Max","Roger", "Computer Science", 3.45, 2021);
+
+        profileTwo.incrementExpectedGraduationYear();
+        System.out.println(profileTwo.expectedYearToGraduate);
+    }
+
+}

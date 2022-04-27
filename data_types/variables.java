3c7cf198af7d3de955ce04c4ab83dcd807374171
Imtiaz Ahmed <ahmedimtiaz.ahmed@gmail.com>
2022-04-27 04:58:04 -0400

    variables

---
+++ data_types/variables.java
@@ -0,0 +1,14 @@
+class studentAge {
+    public static void main(String[] args) {
+        int age = 20;
+        System.out.println("My age is " + age + " years");
+    }
+}
+
+class fortyTwo {
+  public static void main(String args[]) {
+    int meaningOfLife;
+    meaningOfLife = 42;
+    System.out.println(meaningOfLife);
+  }
+}
